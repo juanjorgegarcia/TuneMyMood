d85a2eaaba45a95709c807dfee898dcae4dde3c4
Juan Jorge Garcia <juanjg@Juans-MacBook-Pro.local>
2017-12-13 12:01:44 -0200

    Changes on Index layout, main.js organization.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 $(document).ready(()=>{
     console.log('ready!')
-
     $('#goBtn').click(() => {
         let nome = $('#name').val()
         let genre = $('#genre').val()
         let mood = $('#mood').val()
+        loader()
         createPlaylist(nome,genre,mood)
     })
 
 })
 
+function loader(){
+    $('#goBtn').after(`
+    <div id="loader"  class="progress">
+        <div class="indeterminate"></div>
+    </div>
+`)}
+
+function hideLoader(){
+    $("#loader").css('display', 'none')
+}
 function createPlaylist(nome,genero,moods){
     var params = { genero : genero, moods : moods, name : nome}
     var urlParams = new URLSearchParams(Object.entries(params))
     fetch('http://localhost:5000/playlist?' + urlParams, {
         method : 'GET'
     }).then((res)=>{
-    res.json().then((data)=> {
-        console.log(data)        
-        $('#goBtn').after(`
-        <iframe src="https://open.spotify.com/embed?uri=${data.uri}" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>`)
-    })})
+        res.json().then((data)=> {
+            console.log(data)        
+            $('#goBtn').after(`
+            <iframe src="https://open.spotify.com/embed?uri=${data.uri}" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>`)
+        })
+    hideLoader()
+    })
 }
